3610750b331b44185442eec2eb5723226037c07f
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-08-31 23:43:48 -0400

    Update sceneLauncher.js

--- script/sceneLauncher.js
+++ script/sceneLauncher.js
@@ -1,1 +1,19 @@
+// -- sceneLauncher.js: launches the phaser framework
 
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: function() {}, // preload event does nothing yet
+    create: loadScene
+  }
+};
+
+game = new Phaser.Game(config);
