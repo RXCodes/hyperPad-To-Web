2bdfdc589f3a136fb5a6180d01fe8dd767bed41e
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-08-31 23:39:53 -0400

    Update colorConverter.js

--- script/colorConverter.js
+++ script/colorConverter.js
@@ -1,3 +1,5 @@
+// -- colorConverter.js: converts RGB to/from Hex values - useful for object colors
+
 // convert RGB values to hex
 function rgbToHex(r, g, b) {
   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
@@ -6,7 +8,7 @@ function rgbToHex(r, g, b) {
 // convert color hex values back to RGB
 function hexToRgb(hex) {
   var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  if(result){
+  if (result){
       var r= parseInt(result[1], 16);
       var g= parseInt(result[2], 16);
       var b= parseInt(result[3], 16);
