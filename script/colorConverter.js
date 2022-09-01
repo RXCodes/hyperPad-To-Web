9d232e2a80ef8be93ba4074ec3398ae0ff38e45f
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-08-31 23:10:10 -0400

    Create colorConverter.js

---
+++ script/colorConverter.js
@@ -0,0 +1,16 @@
+// convert RGB values to hex
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+// convert color hex values back to RGB
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if(result){
+      var r= parseInt(result[1], 16);
+      var g= parseInt(result[2], 16);
+      var b= parseInt(result[3], 16);
+      return [r, g, b];
+  } 
+  return null;
+}
