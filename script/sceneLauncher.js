bc190813368f489143fef849e35ba7f50f2c6fbe
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-08-31 23:49:46 -0400

    Update sceneLauncher.js

--- script/sceneLauncher.js
+++ script/sceneLauncher.js
@@ -1,19 +1,34 @@
-// -- sceneLauncher.js: launches the phaser framework
+// -- sceneLauncher.js: launches the phaser framework with global project data
 
-var config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
+// load a level by index or id
+function loadLevel(level) {
+  
+  // kill previous game instance if any
+  
+  
+  // configure phaser scene loader
+  let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { // use the project's gravity settings
+          y: projectBase.yGravity * projectBase.ptm,
+          x: projectBase.xGravity * projectBase.ptm
+        }
+      }
+    },
+    scene: {
+      preload: function() {}, // preload event does nothing yet
+      create: loadLevelHandler(level || 0)
     }
-  },
-  scene: {
-    preload: function() {}, // preload event does nothing yet
-    create: loadScene
-  }
-};
+  };
+  
+  
+  
+}
 
-game = new Phaser.Game(config);
+// load the very first scene
+loadLevel(0);
