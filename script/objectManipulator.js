90e9508651e5a050998abb4db0ac8618858152e0
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-09-10 21:14:23 -0400

    Update objectManipulator.js

--- script/objectManipulator.js
+++ script/objectManipulator.js
@@ -4,14 +4,14 @@
 function setColor(object, r, g, b, a) {
   
   // graphic objects
-  if (object.type == "Empty") {
+  if (object.type == "Graphic") {
     object.setTint(Phaser.Display.Color.GetColor(
       r, g, b
     ));    
   }
   
   // empty objects
-  if (object.type == "Graphic") {
+  if (object.type == "Empty") {
     object.fillColor = Phaser.Display.Color.GetColor(
       r, g, b
     );   
