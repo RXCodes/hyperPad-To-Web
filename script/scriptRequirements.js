380a39c2894e9d4c1aef9b6190bc856301edcc83
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-09-10 20:47:25 -0400

    Create scriptRequirements.js

---
+++ script/scriptRequirements.js
@@ -0,0 +1,9 @@
+let versions = {
+   "v1": [
+     "https://cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.min.js",
+     "https://raw.githubusercontent.com/RXCodes/hyperPad-To-Web/main/script/typeConverter.js",
+     "https://raw.githubusercontent.com/RXCodes/hyperPad-To-Web/main/script/sceneLoader.js",
+     "https://raw.githubusercontent.com/RXCodes/hyperPad-To-Web/main/script/objectManipulator.js"
+  ]
+};
+latestVersion = versions.v1;
