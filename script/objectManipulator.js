d2ba20e8549115b9fa012c2e87b19518af67d07c
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-09-10 20:24:07 -0400

    Create objectManipulator.js

---
+++ script/objectManipulator.js
@@ -0,0 +1,19 @@
+// -- objectManipulator.js: extended functionality to help manipulate objects
+
+// change color of object regardless of type
+function setColor(object, r, g, b, a) {
+  
+  // graphic objects
+  if (object.type == "Empty") {
+    object.setTint(Phaser.Display.Color.GetColor(
+      r, g, b
+    ));    
+  }
+  
+  // empty objects
+  if (object.type == "Graphic") {
+    object.fillColor = Phaser.Display.Color.GetColor(
+      r, g, b
+    ));   
+  }
+}
