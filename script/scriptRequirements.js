986dab08b40095128695fcaa3be3ee149a5bca3d
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-10-13 12:15:31 -0400

    Update scriptRequirements.js

--- script/scriptRequirements.js
+++ script/scriptRequirements.js
@@ -1,8 +1,8 @@
-let versions = {
+window.pathVersions = {
    "v1": [
      "https://cdn.jsdelivr.net/npm/phaser@3.55.2/dist/phaser.min.js",
      "https://raw.githubusercontent.com/RXCodes/hyperPad-To-Web/main/script/sceneLoader.js",
      "https://raw.githubusercontent.com/RXCodes/hyperPad-To-Web/main/script/objectManipulator.js"
   ]
 };
-latestVersion = versions.v1;
+window.latestPathVersion = window.pathVersions.v1;
