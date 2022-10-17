1d83dd21f3eff5ebd8c27496153c8fff6c4f7ee5
RXCodes <61912060+RXCodes@users.noreply.github.com>
2022-10-17 01:53:40 -0400

    Create systemVariableExamples.js

---
+++ script/systemVariableExamples.js
@@ -0,0 +1,13 @@
+// usage of the new system variable
+system.moveBy(object [OBJECT], x [NUMBER], y [NUMBER], relativePosition [BOOLEAN]);
+system.moveToPoint(object [OBJECT], x [NUMBER], y [NUMBER], relativePosition [BOOLEAN]);
+system.setAnchorPoint(object [OBJECT], x [NUMBER], y [NUMBER]);
+system.spawnObject(object [DICTIONARY]);
+system.setFlipX(object [OBJECT], state [BOOLEAN]);
+system.setFlipY(object [OBJECT], state [BOOLEAN]);
+system.setVisibility(object [OBJECT], state [BOOLEAN]);
+system.setZOrder(object [OBJECT], zOrder [NUMBER]);
+system.setRotation(object [OBJECT], angle [NUMBER]);
+system.setScale(object [OBJECT], x [NUMBER], y [NUMBER], usePercentage [BOOL]);
+system.setColor(object [OBJECT], red [NUMBER], green [NUMBER], blue [NUMBER], alpha [NUMBER]);
+system.setBlendMode(object [OBJECT], mode [STRING]);
